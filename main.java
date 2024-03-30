8c37e006bc241fc146dfcac4cda243af2eece24e
Aryan7707 <112489309+Aryan7707@users.noreply.github.com>
2024-03-30 13:49:52 +0530

    Update main.java

--- main.java
+++ main.java
@@ -195,3 +195,24 @@ public class BubbleSort {
                 }
             }
         }
+//************************************************************************
+	    //Aryan Mehta 
+	    import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string1 = scanner.nextLine();
+        String string2 = scanner.nextLine();
+        
+
+
+        if (string1.compareTo(string2) < 0) {
+            System.out.println("-1");
+        } else if (string1.compareTo(string2) > 0) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+
